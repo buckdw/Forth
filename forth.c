434e3a700735cf3c3f224fd768f9d1d7df540d56
Diederick de Buck <diederick.de.buck@platform-42.com>
2025-06-04 11:52:00 +0200

    dddd

--- forth.c
+++ forth.c
@@ -416,7 +416,7 @@ int main() {
     int memory[MEMORY_SIZE];
     char line[LINE_SIZE + 1];
 
-    printf("Diederick's Forth Interpreter (C) - 2025)\n")
+    printf("Diederick's Forth Interpreter (C) - 2025)\n");
     printf("Type 'exit' to quit.\n");
 
     while (true) {
